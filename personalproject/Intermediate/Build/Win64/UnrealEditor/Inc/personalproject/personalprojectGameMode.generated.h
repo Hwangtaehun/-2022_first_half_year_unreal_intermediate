a531315c2cb987eb6b50ff8f3bfeefc0d84e8c5f
Hwangtaehun <r283@naver.com>
2022-07-19 20:49:13 +0900

    Revert "."
    This reverts commit 8930e26354889bb752509c1d578fbe331bb0bb44.

---
+++ personalproject/Intermediate/Build/Win64/UnrealEditor/Inc/personalproject/personalprojectGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PERSONALPROJECT_personalprojectGameMode_generated_h
+#error "personalprojectGameMode.generated.h already included, missing '#pragma once' in personalprojectGameMode.h"
+#endif
+#define PERSONALPROJECT_personalprojectGameMode_generated_h
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_SPARSE_DATA
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_RPC_WRAPPERS
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesApersonalprojectGameMode(); \
+	friend struct Z_Construct_UClass_ApersonalprojectGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ApersonalprojectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/personalproject"), PERSONALPROJECT_API) \
+	DECLARE_SERIALIZER(ApersonalprojectGameMode)
+
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesApersonalprojectGameMode(); \
+	friend struct Z_Construct_UClass_ApersonalprojectGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ApersonalprojectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/personalproject"), PERSONALPROJECT_API) \
+	DECLARE_SERIALIZER(ApersonalprojectGameMode)
+
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	PERSONALPROJECT_API ApersonalprojectGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ApersonalprojectGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(PERSONALPROJECT_API, ApersonalprojectGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ApersonalprojectGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	PERSONALPROJECT_API ApersonalprojectGameMode(ApersonalprojectGameMode&&); \
+	PERSONALPROJECT_API ApersonalprojectGameMode(const ApersonalprojectGameMode&); \
+public:
+
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	PERSONALPROJECT_API ApersonalprojectGameMode(ApersonalprojectGameMode&&); \
+	PERSONALPROJECT_API ApersonalprojectGameMode(const ApersonalprojectGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(PERSONALPROJECT_API, ApersonalprojectGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ApersonalprojectGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ApersonalprojectGameMode)
+
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_9_PROLOG
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_SPARSE_DATA \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_RPC_WRAPPERS \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_INCLASS \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_SPARSE_DATA \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_personalproject_Source_personalproject_personalprojectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PERSONALPROJECT_API UClass* StaticClass<class ApersonalprojectGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_personalproject_Source_personalproject_personalprojectGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
